875b832bdbaca8467d7d5a44ea100150accb683a
Alexander Lytovchenko <flopsi69@gmail.com>
2023-08-03 19:54:41 +0300

    fix

--- src/server/api/character.js
+++ src/server/api/character.js
@@ -1,14 +1,17 @@
 import {
   serverSupabaseServiceRole,
   serverSupabaseUser,
+  serverSupabaseClient,
 } from '#supabase/server';
 
 export default eventHandler(async (event) => {
   const method = getMethod(event);
   const supabase = await serverSupabaseClient(event);
+  console.log('client', supabase);
   const user = await serverSupabaseUser(event);
   console.log('method', method, user, user.id);
-  if (!user?.id) {
+  const id = user?.id;
+  if (!id) {
     throw createError({
       statusMessage: 'Not logged in',
       status: 400,
@@ -26,19 +29,28 @@ export default eventHandler(async (event) => {
 
     console.log('CharSelect', character);
     console.log('CharError', error);
+    if (error) {
+      throw createError({
+        statusMessage: 'Character not found',
+        status: 400,
+      });
+    }
     return character;
   }
 
   if (method === 'POST') {
     const characterData = await readBody(event);
 
-    const { data: character } = await supabase
+    console.log('characterData', characterData);
+
+    const { data: character, error } = await supabase
       .from('characters')
       .insert({ ...characterData, userId: id })
       .select()
       .single();
 
     console.log('CharAdd', character);
+    console.log('CharError', character);
 
     // throw createError({
     //   statusMessage: 'Character added',
